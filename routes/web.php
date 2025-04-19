9bf3d75ff4a7f10e4ac00563f410a03ab6514f34
nnaufal11 <m.naufal110304@gmail.com>
2025-04-19 19:25:41 +0700

    push web

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\LowonganController;
 
 Route::get('/lowongan', [LowonganController::class, 'index'])->name('lowongan.index');
 Route::get('/lowongan/create', [LowonganController::class, 'create'])->name('lowongan.create');
+
