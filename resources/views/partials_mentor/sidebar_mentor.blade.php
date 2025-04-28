aedfbd38873d6f4f0a9e4b3fbbe1ff53a46581da
nnaufal11 <m.naufal110304@gmail.com>
2025-04-28 09:54:10 +0700

    .

--- resources/views/partials_mentor/sidebar_mentor.blade.php
+++ resources/views/partials_mentor/sidebar_mentor.blade.php
@@ -32,8 +32,8 @@
         </li>
 
         <li class="nav-item mb-2">
-            @if (Request::is('mahasiswa/lowongan', 'mahasiswa/lowongan/*'))
-                <a href="/mahasiswa/laporan" class="nav-link text-decoration-none"
+            @if (Request::is('mentor/lowongan', 'mentor/lowongan/*'))
+                <a href="/mentor/laporan" class="nav-link text-decoration-none"
                     style="display: block; padding: 10px 12px; border-radius: 6px; background-color: #ffffff; border: 1px solid #e74c3c;">
                     <span style="color: #e74c3c; font-weight: 600;">
                         <img src="{{ asset('assets/icons/lowongan-icon.svg') }}" alt="Lowongan" width="18"
